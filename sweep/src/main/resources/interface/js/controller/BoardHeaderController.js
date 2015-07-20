8d5c151bbad4e92ea10fc11cdfbb394ec689cc9d
Abhimanyu Babbar <abhimanyu.babbar89@gmail.com>
2015-07-20 18:51:04 +0200

    Code style fix for the header controller.

--- sweep/src/main/resources/interface/js/controller/BoardHeaderController.js
+++ sweep/src/main/resources/interface/js/controller/BoardHeaderController.js
@@ -2,80 +2,88 @@
 
 angular.module('app')
 
-    .controller('BoardHeaderController', ['$log', '$scope', 'common', function ($log, $scope, common) {
+    .controller('BoardHeaderController', ['$log', 'common', BoardHeaderController])
+    .directive('fileUploader', ['$log', 'gvodService', 'sweepService', fileUploader])
+    .directive('clickDirective', ['$log', clickDirective]);
 
-        function _initScope(scope) {
 
-            $log.info('Board Header Controller Initialized.');
-            scope.routeTo = function (path) {
-                common.routeTo(path);
-            };
-            
-            scope.searchObj ={
-                searchTerm:  null
-            };
+// MAIN HEADER CONTROLLER.
+function BoardHeaderController($log, common) {
+
+
+    $log.info('Board Header Controller Initialized.');
+    var self = this;
+
+    self.routeTo = function (path) {
+        common.routeTo(path);
+    };
+
+    self.searchObj = {
+        searchTerm: null
+    };
+
+    self.search = function (searchTerm) {
+
+        if (this.searchForm.$valid) {
+            $log.info('search form valid');
+            common.routeTo('/search/' + searchTerm);
         }
-        
-        
-        $scope.search = function(searchTerm){
-          
-            if(this.searchForm.$valid){
-                $log.info('search form valid');
-                common.routeTo('/search/'+ searchTerm);
-            }
-        };
-
-        _initScope($scope);
-    }])
-
-    .directive('fileUploader', ['$log','gvodService', 'sweepService', function ($log,gvodService, sweepService) {
-
-        // Upload the file to the system.
-        return{
-            
-            restrict: 'A',
-            link: function (scope, element, attributes) {
-
-                element.bind('change', function (changeEvent) {
-
-                    var reader = new FileReader();
-                    reader.onload = function (loadEvent) {
+    }
+}
 
 
-                        $log.debug(" Going to load the file content ... ");
+// DIRECTIVE EXPLAINING
 
-                        var serverData = {
-                            info: loadEvent.target.result
-                        };
+function fileUploader($log, gvodService, sweepService) {
 
-                        var serverInfo = JSON.parse(serverData.info);
 
-                        $log.debug(serverInfo);
+    // Upload the file to the system.
+    return {
 
-                        gvodService.setServer(serverInfo.gvod);
-                        sweepService.setServer(serverInfo.sweep);
+        restrict: 'A',
+        link: function (scope, element, attributes) {
 
-                        element.val("");
+            element.bind('change', function (changeEvent) {
+
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+
+                    $log.debug(" Going to load the file content ... ");
+
+                    var serverData = {
+                        info: loadEvent.target.result
                     };
 
-                    reader.readAsText(changeEvent.target.files[0]);
-                });
-            }
+                    var serverInfo = JSON.parse(serverData.info);
+                    $log.debug(serverInfo);
+
+                    gvodService.setServer(serverInfo.gvod);
+                    sweepService.setServer(serverInfo.sweep);
+
+                    element.val("");
+                };
+
+                reader.readAsText(changeEvent.target.files[0]);
+            });
         }
-    }])
+    }
+}
 
-    .directive('clickDirective', ['$log', function ($log) {
 
-        return {
-            restrict: 'A',
-            link: function (scope, element, attributes) {
+// DIRECTIVE TO REDIRECT CLICK.
 
-                element.bind('click', function (clickEvent) {
-                    var uploaderElement = angular.element(document.querySelector("#fileUploader"));
-                    uploaderElement.trigger('click');
-                });
+function clickDirective($log) {
 
-            }
+    return {
+        restrict: 'A',
+        link: function (scope, element, attributes) {
+
+            element.bind('click', function (clickEvent) {
+                var uploaderElement = angular.element(document.querySelector("#fileUploader"));
+                uploaderElement.trigger('click');
+            });
+
         }
+    }
+}
 
-    }]);
