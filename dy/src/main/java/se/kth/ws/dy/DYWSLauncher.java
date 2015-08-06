740dece71e4e4ef7043739235540ad771ac46044
Alex Ormenisan <ormenisan.adrian@gmail.com>
2015-08-06 12:38:55 +0200

    dy version bump - 2.2 sweep - 2.2 gvod - 2.2

--- dy/src/main/java/se/kth/ws/dy/DYWSLauncher.java
+++ dy/src/main/java/se/kth/ws/dy/DYWSLauncher.java
@@ -28,10 +28,10 @@ import java.net.InetAddress;
 import java.util.EnumSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.kth.ws.sweep.BootstrapNodes;
 import se.kth.ws.sweep.core.SweepSyncComponent;
-import se.sics.gvod.bootstrap.cclient.CaracalPSManagerComp;
-import se.sics.gvod.bootstrap.server.peermanager.PeerManagerPort;
-import se.sics.gvod.bootstrap.server.peermanager.msg.CaracalReady;
+import se.sics.caracaldb.MessageRegistrator;
+import se.sics.gvod.common.util.VoDHeartbeatServiceEnum;
 import se.sics.gvod.config.GradientConfiguration;
 import se.sics.gvod.config.SearchConfiguration;
 import se.sics.gvod.manager.toolbox.GVoDSyncI;
@@ -40,10 +40,10 @@ import se.sics.gvod.system.HostManagerComp;
 import se.sics.gvod.system.HostManagerConfig;
 import se.sics.kompics.Component;
 import se.sics.kompics.ComponentDefinition;
+import se.sics.kompics.Fault;
 import se.sics.kompics.Handler;
 import se.sics.kompics.Init;
 import se.sics.kompics.Kompics;
-import se.sics.kompics.Positive;
 import se.sics.kompics.Start;
 import se.sics.kompics.Stop;
 import se.sics.kompics.network.Network;
@@ -51,15 +51,22 @@ import se.sics.kompics.network.netty.NettyInit;
 import se.sics.kompics.network.netty.NettyNetwork;
 import se.sics.kompics.timer.Timer;
 import se.sics.kompics.timer.java.JavaTimer;
+import se.sics.ktoolbox.cc.bootstrap.CCBootstrapComp;
+import se.sics.ktoolbox.cc.bootstrap.CCBootstrapPort;
+import se.sics.ktoolbox.cc.bootstrap.msg.CCReady;
+import se.sics.ktoolbox.cc.common.config.CaracalClientConfig;
+import se.sics.ktoolbox.cc.common.op.CCSimpleReady;
+import se.sics.ktoolbox.cc.heartbeat.CCHeartbeatComp;
+import se.sics.ktoolbox.cc.heartbeat.CCHeartbeatPort;
 import se.sics.ktoolbox.ipsolver.IpSolverComp;
 import se.sics.ktoolbox.ipsolver.IpSolverPort;
 import se.sics.ktoolbox.ipsolver.msg.GetIp;
 import se.sics.ms.common.ApplicationSelf;
-import se.sics.ms.configuration.MsConfig;
 import se.sics.ms.net.SerializerSetup;
 import se.sics.ms.ports.UiPort;
 import se.sics.ms.search.SearchPeer;
 import se.sics.ms.search.SearchPeerInit;
+import se.sics.ms.util.HeartbeatServiceEnum;
 import se.sics.p2ptoolbox.aggregator.network.AggregatorSerializerSetup;
 import se.sics.p2ptoolbox.chunkmanager.ChunkManagerConfig;
 import se.sics.p2ptoolbox.chunkmanager.ChunkManagerSerializerSetup;
@@ -85,45 +92,46 @@ public class DYWSLauncher extends ComponentDefinition {
         ipType = setIpType;
     }
 
-    private Component ipSolver;
-    private Component network;
-    private Component timer;
-    private Component sweep;
-    private Component sweepSync;
-    private Component gvod;
-    private Component caracalProxy;
+    private Component timerComp;
+    private Component ipSolverComp;
+    private Component networkComp;
+    private Component caracalClientComp;
+    private Component heartbeatComp;
+    private Component sweepHostComp;
+    private Component sweepSyncComp;
+    private Component vodHostComp;
     private DYWS dyWS;
     private Config config;
     private SystemConfig systemConfig;
+    private CaracalClientConfig ccConfig;
 
     private SettableFuture<GVoDSyncI> gvodSyncIFuture;
     private SweepSyncI sweepSyncI;
 
     private HostManagerConfig gvodConfig;
+    
+    byte[] vodSchemaId = null;
     //**************************************************************************
 
     public DYWSLauncher() {
         LOG.info("initiating...");
         if (ipType == null) {
             LOG.error("launcher logic error - ipType not set");
-            throw new RuntimeException("launcher logic error - ipType not set");
+            System.exit(1);
         }
-
+        gvodSyncIFuture = SettableFuture.create();
         registerSerializers();
 
         subscribe(handleStart, control);
         subscribe(handleStop, control);
-
-        ipSolver = create(IpSolverComp.class, new IpSolverComp.IpSolverInit());
     }
 
     Handler handleStart = new Handler<Start>() {
         @Override
         public void handle(Start event) {
             LOG.info("starting: solvingIp");
-            Positive<IpSolverPort> ipSolverPort = ipSolver.getPositive(IpSolverPort.class);
-            subscribe(handleGetIp, ipSolverPort);
-            trigger(new GetIp.Req(EnumSet.of(ipType)), ipSolverPort);
+
+            phase1();
         }
     };
 
@@ -133,6 +141,35 @@ public class DYWSLauncher extends ComponentDefinition {
             LOG.info("stopping...");
         }
     };
+
+    @Override
+    public Fault.ResolveAction handleFault(Fault fault) {
+        LOG.error("exception:{} shutting down", fault.getCause());
+        System.exit(1);
+        return Fault.ResolveAction.RESOLVED;
+    }
+    
+    private void registerSerializers() {
+        MessageRegistrator.register();
+        int currentId = 128;
+        currentId = BasicSerializerSetup.registerBasicSerializers(currentId);
+        currentId = CroupierSerializerSetup.registerSerializers(currentId);
+        currentId = GradientSerializerSetup.registerSerializers(currentId);
+        currentId = ElectionSerializerSetup.registerSerializers(currentId);
+        currentId = AggregatorSerializerSetup.registerSerializers(currentId);
+        currentId = ChunkManagerSerializerSetup.registerSerializers(currentId);
+        currentId = SerializerSetup.registerSerializers(currentId);
+        currentId = GVoDSerializerSetup.registerSerializers(currentId);
+    }
+
+    private void phase1() {
+        timerComp = create(JavaTimer.class, Init.NONE);
+        trigger(Start.event, timerComp.control());
+        ipSolverComp = create(IpSolverComp.class, new IpSolverComp.IpSolverInit());
+        trigger(Start.event, ipSolverComp.control());
+        subscribe(handleGetIp, ipSolverComp.getPositive(IpSolverPort.class));
+        trigger(new GetIp.Req(EnumSet.of(ipType)), ipSolverComp.getPositive(IpSolverPort.class));
+    }
 
     public Handler handleGetIp = new Handler<GetIp.Resp>() {
         @Override
@@ -149,54 +186,77 @@ public class DYWSLauncher extends ComponentDefinition {
 
             config = ConfigFactory.load();
             systemConfig = new SystemConfig(config, ip);
+            ccConfig = new CaracalClientConfig(config);
             gvodConfig = new HostManagerConfig(config, ip);
 
-            timer = create(JavaTimer.class, Init.NONE);
-            trigger(Start.event, timer.control());
-            network = create(NettyNetwork.class, new NettyInit(systemConfig.self));
-            trigger(Start.event, network.control());
-            
-            caracalConnectPhase();
+            phase2();
         }
     };
 
-    private void registerSerializers() {
-        int currentId = 128;
-        currentId = BasicSerializerSetup.registerBasicSerializers(currentId);
-        currentId = CroupierSerializerSetup.registerSerializers(currentId);
-        currentId = GradientSerializerSetup.registerSerializers(currentId);
-        currentId = ElectionSerializerSetup.registerSerializers(currentId);
-        currentId = AggregatorSerializerSetup.registerSerializers(currentId);
-        currentId = ChunkManagerSerializerSetup.registerSerializers(currentId);
-        currentId = SerializerSetup.registerSerializers(currentId);
-        currentId = GVoDSerializerSetup.registerSerializers(currentId);
+    private void phase2() {
+        connectNetwork();
+        connectCaracalClient();
+        connectHeartbeat();
+        subscribe(handleCaracalReady, caracalClientComp.getPositive(CCBootstrapPort.class));
+        subscribe(handleHeartbeatReady, heartbeatComp.getPositive(CCHeartbeatPort.class));
+    }
+    
+    private void connectNetwork() {
+        networkComp = create(NettyNetwork.class, new NettyInit(systemConfig.self));
+        trigger(Start.event, networkComp.control());
+    }
+    
+    private void connectCaracalClient() {
+        
+        caracalClientComp = create(CCBootstrapComp.class, new CCBootstrapComp.CCBootstrapInit(systemConfig, ccConfig, BootstrapNodes.readCaracalBootstrap(config)));
+        connect(caracalClientComp.getNegative(Timer.class), timerComp.getPositive(Timer.class));
+        connect(caracalClientComp.getNegative(Network.class), networkComp.getPositive(Network.class));
+        trigger(Start.event, caracalClientComp.control());
     }
 
-    private void caracalConnectPhase() {
-        //TODO Alex should create and start only on open nodes
-        caracalProxy = create(CaracalPSManagerComp.class, new CaracalPSManagerComp.CaracalPSManagerInit(gvodConfig.getCaracalPSManagerConfig()));
-        connect(caracalProxy.getNegative(Timer.class), timer.getPositive(Timer.class));
-        trigger(Start.event, caracalProxy.control());
-
-        subscribe(handleCaracalReady, caracalProxy.getPositive(PeerManagerPort.class));
+    private void connectHeartbeat() {
+        heartbeatComp = create(CCHeartbeatComp.class, new CCHeartbeatComp.CCHeartbeatInit(systemConfig, ccConfig));
+        connect(heartbeatComp.getNegative(Timer.class), timerComp.getPositive(Timer.class));
+        connect(heartbeatComp.getNegative(CCBootstrapPort.class), caracalClientComp.getPositive(CCBootstrapPort.class));
+        trigger(Start.event, heartbeatComp.control());
     }
 
-    private Handler handleCaracalReady = new Handler<CaracalReady>() {
-
+    private Handler handleCaracalReady = new Handler<CCReady>() {
         @Override
-        public void handle(CaracalReady event) {
-            LOG.info("starting: system");
-            connectComponents();
+        public void handle(CCReady event) {
+            LOG.info("starting: received schemas");
+            vodSchemaId = event.caracalSchemaData.getId("gvod.metadata");
+            if (vodSchemaId == null) {
+                LOG.error("exception:vod schema undefined shutting down");
+                System.exit(1);
+            }
         }
     };
 
-    private void connectComponents() {
+    private Handler handleHeartbeatReady = new Handler<CCSimpleReady>() {
+        @Override
+        public void handle(CCSimpleReady e) {
+            LOG.info("starting: system");
+            phase3();
+        }
+    };
+    
+    private void phase3() {
         connectSweep();
         connectSweepSync();
-        connectGVoD();
+        connectVoDHost();
         startWebservice();
     }
-
+    
+    private void connectVoDHost() {
+        vodHostComp = create(HostManagerComp.class, new HostManagerComp.HostManagerInit(gvodConfig, gvodSyncIFuture, vodSchemaId));
+        connect(vodHostComp.getNegative(Network.class), networkComp.getPositive(Network.class));
+        connect(vodHostComp.getNegative(Timer.class), timerComp.getPositive(Timer.class));
+        connect(vodHostComp.getNegative(CCBootstrapPort.class), caracalClientComp.getPositive(CCBootstrapPort.class));
+        connect(vodHostComp.getNegative(CCHeartbeatPort.class), heartbeatComp.getPositive(CCHeartbeatPort.class));
+        trigger(Start.event, vodHostComp.control());
+    }
+    
     private void connectSweep() {
         GradientConfig gradientConfig = new GradientConfig(config);
         CroupierConfig croupierConfig = new CroupierConfig(config);
@@ -206,28 +266,19 @@ public class DYWSLauncher extends ComponentDefinition {
 
         //TODO Abhi - why aren't you building this applicationSelf in SearchPeer and instead risk me handing this reference to someone else - shared object problem
         ApplicationSelf applicationSelf = new ApplicationSelf(systemConfig.self);
-        sweep = create(SearchPeer.class, new SearchPeerInit(applicationSelf, systemConfig, croupierConfig,
+        sweepHostComp = create(SearchPeer.class, new SearchPeerInit(applicationSelf, systemConfig, croupierConfig,
                 SearchConfiguration.build(), GradientConfiguration.build(),
                 chunkManagerConfig, gradientConfig, electionConfig, treeGradientConfig));
-        connect(timer.getPositive(Timer.class), sweep.getNegative(Timer.class));
-        connect(network.getPositive(Network.class), sweep.getNegative(Network.class));
-
-        trigger(Start.event, sweep.control());
+        connect(sweepHostComp.getNegative(Timer.class), timerComp.getPositive(Timer.class));
+        connect(sweepHostComp.getNegative(Network.class), networkComp.getPositive(Network.class));
+        trigger(Start.event, sweepHostComp.control());
     }
 
     private void connectSweepSync() {
-        sweepSync = create(SweepSyncComponent.class, Init.NONE);
-        sweepSyncI = (SweepSyncI) sweepSync.getComponent();
-        connect(sweep.getPositive(UiPort.class), sweepSync.getNegative(UiPort.class));
-        trigger(Start.event, sweepSync.control());
-    }
-
-    private void connectGVoD() {
-        gvodSyncIFuture = SettableFuture.create();
-        gvod = create(HostManagerComp.class, new HostManagerComp.HostManagerInit(gvodConfig, caracalProxy, gvodSyncIFuture));
-        connect(gvod.getNegative(Network.class), network.getPositive(Network.class));
-        connect(gvod.getNegative(Timer.class), timer.getPositive(Timer.class));
-        trigger(Start.event, gvod.control());
+        sweepSyncComp = create(SweepSyncComponent.class, Init.NONE);
+        sweepSyncI = (SweepSyncI) sweepSyncComp.getComponent();
+        connect(sweepSyncComp.getNegative(UiPort.class), sweepHostComp.getPositive(UiPort.class));
+        trigger(Start.event, sweepSyncComp.control());
     }
 
     private void startWebservice() {
@@ -247,16 +298,17 @@ public class DYWSLauncher extends ComponentDefinition {
     }
 
     public static void main(String[] args) throws IOException {
+        HeartbeatServiceEnum.CROUPIER.setServiceId((byte) 1);
+        VoDHeartbeatServiceEnum.CROUPIER.setServiceId((byte) 2);
         GetIp.NetworkInterfacesMask setIpType = GetIp.NetworkInterfacesMask.PUBLIC;
         if (args.length == 1 && args[0].equals("-tenDot")) {
             setIpType = GetIp.NetworkInterfacesMask.TEN_DOT_PRIVATE;
         }
+        DYWSLauncher.setIpType(setIpType);
+
         if (Kompics.isOn()) {
             Kompics.shutdown();
         }
-//        MsConfig.init(args);
-        System.setProperty("java.net.preferIPv4Stack", "true");
-        DYWSLauncher.setIpType(setIpType);
         Kompics.createAndStart(DYWSLauncher.class, Runtime.getRuntime().availableProcessors(), 20); // Yes 20 is totally arbitrary
         try {
             Kompics.waitForTermination();
